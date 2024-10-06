83dee1790051f7943f508208e6736c52e64321e0
sieugene <sieugene@mail.ru>
2024-10-06 14:48:02 +0900

    update docker, multiple upload, folder project split, add requirements

---
+++ mokuro-server/Readme.md
@@ -0,0 +1,20 @@
+# Draft ...
+
+## Description
+
+...
+
+## Install
+```
+$ pip install -r requirements.txt
+```
+
+## Run
+```
+python app.py
+```
+
+## Docker compose
+```bash
+docker-compose up -d
+```
--- mokuro-server/app.py
+++ mokuro-server/app.py
@@ -1,31 +1,45 @@
 import os
 import uuid
 import subprocess
+import shutil
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'projects/uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+BASE_UPLOAD_FOLDER = 'projects/uploads'
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
+@app.route('/upload/<string:title>', methods=['POST'])
+def upload_file(title):
+
+    project_folder = os.path.join(BASE_UPLOAD_FOLDER, title)
+    upload_folder = os.path.join(project_folder, "images")
+
+    if os.path.exists(project_folder):
+        shutil.rmtree(project_folder)
+
+    os.makedirs(upload_folder, exist_ok=True)
+
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
 
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
+    files = request.files.getlist('file')
+    if not files:
+        return jsonify({"error": "No selected files"}), 400
 
-    file_extension = os.path.splitext(file.filename)[1]
-    new_filename = f"{uuid.uuid4()}{file_extension}"
-    file_path = os.path.join(UPLOAD_FOLDER, new_filename)
-    file.save(file_path)
+    file_paths = []
+    for file in files:
+        if file.filename == '':
+            return jsonify({"error": "One or more files have no selected filename"}), 400
 
-    manga_volume_path = os.path.dirname(file_path)
-    subprocess.Popen(['mokuro', manga_volume_path, '--force_cpu' ,'--disable_confirmation'])
+        file_extension = os.path.splitext(file.filename)[1]
+        new_filename = f"{uuid.uuid4()}{file_extension}"
+        file_path = os.path.join(upload_folder, new_filename)
+        file.save(file_path)
+        file_paths.append(file_path)
 
-    return jsonify({"message": "File uploaded and Mokuro started."}), 200
+    subprocess.Popen(['mokuro', upload_folder, '--force_cpu', '--disable_confirmation'])
+
+    return jsonify({"message": f"{len(file_paths)} files uploaded and Mokuro started for '{title}'."}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
